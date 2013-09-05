ab3ded84726fe2ecbe10858866f8797407f113e6
Xuemin Guan <xuemin.guan@db.com>
2013-09-05 14:30:01 +0100

    adding new sbt launcher

--- sbt.bat
+++ sbt.bat
@@ -1,3 +1,5 @@
+@echo off
 set SCRIPT_DIR=%~dp0
-java -Xmx512M -jar "%SCRIPT_DIR%sbt-launch.jar" %*
 
+set JAVA_OPTS=-Dhttp.proxyHost=surf-proxy.intranet.db.com -Dhttp.proxyPort=8080 -Dhttp.nonProxyHosts="*.db.com|localhost|127.0.0.1"
+java %JAVA_OPTS% %SBT_OPTS% -Xmx1024m -Xss4M -XX:MaxPermSize=512m -jar "%SCRIPT_DIR%sbt-launch.jar" %*
