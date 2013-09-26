6d54b6a7a9680d51d183d821aac2b63bd8364b54
Xuemin Guan <xuemin.guan@db.com>
2013-09-26 13:02:05 +0100

    implementing size

--- exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
@@ -13,5 +13,9 @@ class TreeSpec extends Specification {
     "give 2 for a two Leaf tree" in {
       Tree.size(Branch(Leaf(1), Leaf(2))) must_== 2
     }
+
+    "count the size of a nested tree" in {
+      Tree.size(Branch(Leaf(1), Branch(Leaf(2), Branch(Leaf(3), Leaf(4))))) must_== 4
+    }
   }
 }
