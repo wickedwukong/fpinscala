8953e67bd0f04cdea7bf81cba86bee1423075abb
Xuemin Guan <gxuemin@gmail.com>
2013-09-26 22:19:02 +0100

    implemented max for tree

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -1,5 +1,7 @@
 package fpinscala.datastructures
 
+import scala.runtime.RichInt
+
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
@@ -10,6 +12,15 @@ object Tree {
     tree match {
       case Leaf(_) => 1
       case Branch(leftTree, rightTree) => size(leftTree) + size(rightTree)
+    }
+  }
+
+  def max(tree: Tree[Int]): Int = {
+    tree match {
+      case Leaf(a) => a
+      case Branch(leftTree, rightTree) => {
+        Tree.max(leftTree) max Tree.max(rightTree)
+      }
     }
   }
 }
--- exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
@@ -16,4 +16,22 @@ class TreeSpec extends Specification {
       Tree.size(Branch(Leaf(1), Branch(Leaf(2), Branch(Leaf(3), Leaf(4))))) must_== 4
     }
   }
+
+  "max" should {
+    "give the value of the single leaf tree" in {
+      Tree.max(Leaf(100)) must_== 100
+    }
+
+    "give the higher value of a two leaf tree" in {
+      Tree.max(Branch(Leaf(100), Leaf(1))) must_== 100
+      Tree.max(Branch(Leaf(100), Leaf(100))) must_== 100
+      Tree.max(Branch(Leaf(1), Leaf(100))) must_== 100
+    }
+
+    "give the higest value of a nested tree" in {
+      Tree.max(Branch(Leaf(100), Branch(Leaf(200), Leaf(300)))) must_== 300
+      Tree.max(Branch(Branch(Leaf(200), Leaf(300)), Leaf(100))) must_== 300
+      Tree.max(Branch(Branch(Leaf(200), Leaf(300)), Branch(Leaf(200), Leaf(400)))) must_== 400
+    }
+  }
 }
