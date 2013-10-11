749f9ca6038a2f497c2d9a83d691b5ccc0426cf7
Xuemin Guan <xuemin.guan@db.com>
2013-10-11 14:20:27 +0100

    implementing sequence.

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -5,15 +5,15 @@ sealed trait Option[+A] {
     case None => None
     case Some(a) => Some(f(a))
   }
-  
-  def getOrElse[B>:A](default: => B): B = this match {
+
+  def getOrElse[B >: A](default: => B): B = this match {
     case None => default
     case Some(a) => a
   }
-  
-  def flatMap[B](f: A => Option[B]): Option[B] = 
+
+  def flatMap[B](f: A => Option[B]): Option[B] =
     map(f) getOrElse None
-  
+
   /*
   Of course, we can also implement `flatMap` with explicit pattern matching.
   */
@@ -21,29 +21,32 @@ sealed trait Option[+A] {
     case None => None
     case Some(a) => f(a)
   }
-  
-  def orElse[B>:A](ob: => Option[B]): Option[B] = 
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
     this map (Some(_)) getOrElse ob
-  
+
   /*
   Again, we can implement this with explicit pattern matching. 
   */
-  def orElse_1[B>:A](ob: => Option[B]): Option[B] = this match {
-    case None => ob 
+  def orElse_1[B >: A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
     case _ => this
   }
-  
+
   def filter(f: A => Boolean): Option[A] = this match {
     case Some(a) if f(a) => this
     case _ => None
   }
+
   /*
   This can also be defined in terms of `flatMap`.
   */
   def filter_1(f: A => Boolean): Option[A] =
     flatMap(a => if (f(a)) Some(a) else None)
 }
+
 case class Some[+A](get: A) extends Option[A]
+
 case object None extends Option[Nothing]
 
 object Option {
@@ -53,7 +56,9 @@ object Option {
       val y = 42 + 5
       x + y
     }
-    catch { case e: Exception => 43 }
+    catch {
+      case e: Exception => 43
+    }
   }
 
   def mean(xs: Seq[Double]): Option[Double] =
@@ -61,7 +66,7 @@ object Option {
     else Some(xs.sum / xs.length)
 
   import java.util.regex._
-  
+
   def pattern(s: String): Option[Pattern] =
     try {
       Some(Pattern.compile(s))
@@ -69,15 +74,15 @@ object Option {
       case e: PatternSyntaxException => None
     }
 
-  def mkMatcher(pat: String): Option[String => Boolean] = 
+  def mkMatcher(pat: String): Option[String => Boolean] =
     pattern(pat) map (p => (s: String) => p.matcher(s).matches) // The details of this API don't matter too much, but `p.matcher(s).matches` will check if the string `s` matches the pattern `p`.
 
-  def mkMatcher_1(pat: String): Option[String => Boolean] = 
+  def mkMatcher_1(pat: String): Option[String => Boolean] =
     for {
       p <- pattern(pat)
     } yield ((s: String) => p.matcher(s).matches)
-  
-  def doesMatch(pat: String, s: String): Option[Boolean] = 
+
+  def doesMatch(pat: String, s: String): Option[Boolean] =
     for {
       p <- mkMatcher_1(pat)
     } yield p(s)
@@ -89,12 +94,13 @@ object Option {
     } yield f(s) && g(s)
 
   def bothMatch_1(pat: String, pat2: String, s: String): Option[Boolean] =
-    mkMatcher(pat) flatMap (f => 
-    mkMatcher(pat2) map     (g => 
-    f(s) && g(s)))
+    mkMatcher(pat) flatMap (f =>
+      mkMatcher(pat2) map (g =>
+        f(s) && g(s)))
+
   def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
     a.flatMap(aValue => b.map(bValue => f(aValue, bValue)))
   }
 
@@ -102,7 +108,8 @@ object Option {
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = {
     a match {
-      case head :: tail => head.map(h => List(h) :: sequence(tail))
+      case Nil => Some(Nil)
+      case head :: tail => { head.flatMap(hh => sequence(tail).map(hh :: _))}
     }
   }
 
