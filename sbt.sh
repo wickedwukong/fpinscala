61af3f7320203a4aceb6e099a1cb4b171d5b36be
Xuemin Guan <xuemin.guan@db.com>
2013-10-29 13:27:26 +0000

    committing shell file.

---
+++ sbt.sh
@@ -0,0 +1,8 @@
+WORKSPACE=~
+if [ "$(uname)" == "MINGW32_NT-6.1" ]; then
+	WORKSPACE=$LOCALAPPDATA
+fi
+
+JAVA_OPTS="-Dbuild.number=$BUILD_NUMBER -Dsbt.global.base=$WORKSPACE/.sbt -Dsbt.boot.directory=$WORKSPACE/.sbt/boot -Dsbt.ivy.home=$WORKSPACE/.ivy2 -Dhttp.proxyHost=surf-proxy.intranet.db.com -Dhttp.proxyPort=8080 -Dhttp.nonProxyHosts=*.db.com -Xmx1024M -XX:MaxPermSize=512m -XX:ReservedCodeCacheSize=128m"
+
+exec java $JAVA_OPTS -jar sbt-launch-0.12.2.jar "$@"
