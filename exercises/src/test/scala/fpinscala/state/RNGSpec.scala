cc7f02983f01b8019d436e8147fb19d69c5ad5e2
Xuemin Guan <gxuemin@gmail.com>
2013-12-11 22:51:30 +0000

    using scala check to test RNG

--- exercises/src/test/scala/fpinscala/state/RNGSpec.scala
+++ exercises/src/test/scala/fpinscala/state/RNGSpec.scala
@@ -1,15 +1,19 @@
 package fpinscala.state
 
 import org.specs2.mutable.Specification
+import org.specs2.ScalaCheck
+import org.scalacheck._
 
-class RNGSpec extends Specification {
+class RNGSpec extends Specification with ScalaCheck {
 
   "positiveRNG" should {
     "always generate positive integers" in {
-      1.until(100).map{_ =>
-        RNG.positiveInt(RNG.simple(System.currentTimeMillis()))._1 must be_>=(0)
+      "different seeds" ! check {
+        (seed: Long) => {
+          RNG.positiveInt(RNG.simple(seed))._1 must be_>=(0)
+
+        }
       }
     }
   }
-
 }
--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,8 @@ object FPInScalaBuild extends Build {
     scalaVersion := "2.10.2",
     resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
     libraryDependencies ++= Seq(
-      "org.specs2" %% "specs2" % "2.0" % "test",
+      "org.specs2" %% "specs2" % "2.3.6" % "test",
+      "org.scalacheck" %% "scalacheck" % "1.11.1" % "test",
       "com.typesafe.akka" %% "akka-actor" % "2.1.0"
     )
   )
