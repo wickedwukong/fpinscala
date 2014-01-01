8ba97a38bc67aafb0bc2ba1499aac21175e9d572
Xuemin Guan <gxuemin@gmail.com>
2014-01-01 19:21:39 +0000

    implemented flatMap, and reimplemented map using flatMap

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -142,19 +142,24 @@ object RNG {
   }
 }
 
+
+import State._
+
 case class State[S, +A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] = {
-      State(s => {
-        val (a, s1) = run(s)
-        (f(a),s1)
-      })
+    flatMap(a => unit(f(a)))
+
   }
 
   def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
     sys.error("todo")
 
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    sys.error("todo")
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(
+        s => {
+          val (a, s1) = run(s)
+          f(a).run(s1)
+        }
+      )
 }
 
 sealed trait Input
@@ -167,6 +172,8 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
   type Rand[A] = State[RNG, A]
+
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
 
   def simulateMachine(inputs: List[Input]): State[Machine, Int] = sys.error("todo")
 }
