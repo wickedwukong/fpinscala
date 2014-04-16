3a8ebb1bd3f8eb208eb2cbfcfba39adb09ab7777
Xuemin Guan <xuemin.guan@db.com>
2014-04-16 15:59:09 +0100

    implemenint foldMapV

--- exercises/src/main/scala/fpinscala/monoids/Monoid.scala
+++ exercises/src/main/scala/fpinscala/monoids/Monoid.scala
@@ -101,13 +101,13 @@ object Monoid {
     foldMap(as, dual(endoMonoid[B]))(a => b => f(b,a))(z)
 
   def foldMapV[A, B](as: IndexedSeq[A], m: Monoid[B])(f: A => B): B = {
-    if (as.length == 0)
+    if (as.length == 0) {
       m.zero
-    else if (as.length == 1)
+    } else if (as.length == 1) {
       f(as(0))
-    else {
-      val (l, r) = as.splitAt(as.length / 2)
-      m.op(foldMapV(l, m)(f), foldMapV(r, m)(f))
+    } else {
+      val (as1, as2) = as.splitAt(as.length / 2)
+      m.op(foldMapV(as1, m)(f), foldMapV(as2, m)(f))
     }
   }
 
