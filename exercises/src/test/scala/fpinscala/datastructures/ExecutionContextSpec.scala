71c49c86ea6c81e75a19670b38fda1891353b6ff
Xuemin Guan <gxuemin@gmail.com>
2014-09-27 00:29:25 +0100

    have the return type different from f return type so that we can support both T and Future[T]

--- exercises/src/test/scala/fpinscala/datastructures/ExecutionContextSpec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/ExecutionContextSpec.scala
@@ -9,7 +9,8 @@ import scala.util.Success
 
 trait ExecutionContext[T] {
     def execute(f: () => T): Either[Future[T], T]
- }
+}
+
 
  class SyncExecutionContext[T] extends ExecutionContext[T] {
     def execute(f: () => T): Either[Future[T], T] = {
@@ -22,6 +23,22 @@ trait ExecutionContext[T] {
       Left(Future{f()})
     }
  }
+
+trait ExecutionContext2[T, U] {
+  def execute(f: () => T): U
+}
+
+class SyncExecutionContext2[T] extends ExecutionContext2[T, T] {
+  override def execute(f: () => T): T = {
+    f()
+  }
+}
+
+class AsyncExecutionContext2[T] extends ExecutionContext2[T, Future[T]] {
+  def execute(f: () => T): Future[T] = {
+    Future{f()}
+  }
+}
 
 
 class ExecutionContextSpec extends Specification {
@@ -41,7 +58,25 @@ class ExecutionContextSpec extends Specification {
 
       leftFutureResult.map(_.value) must_== Left(Some(Success("Hello")))
     }
+  }
+}
 
+class ExecutionContextSpec2 extends Specification {
+  def executeInContext2[U](f: () => String, context: ExecutionContext2[String, U]) = {
+    context.execute(f)
   }
 
+  "Execution context" should {
+    "execute function synconousely" in {
+      val f = () => "Hello"
+      executeInContext2[String](f, new SyncExecutionContext2[String]) must_== "Hello"
+    }
+
+  "execute function asynconousely" in {
+      val f = () => "Hello"
+      val futureResult: Future[String] = executeInContext2[Future[String]](f, new AsyncExecutionContext2[String]())
+
+      futureResult.value must_== Some(Success("Hello"))
+    }
+  }
 }
